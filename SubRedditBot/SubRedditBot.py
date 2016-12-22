a71c8409ee4240914b665630b645a98885603ec5
Mitchell Fujs <mitchell.fujs@gmail.com>
2016-12-22 17:26:18 -0500

    Minor changes
    Add space acceptance in future

--- SubRedditBot/SubRedditBot.py
+++ SubRedditBot/SubRedditBot.py
@@ -2,7 +2,7 @@ import discord
 import asyncio
 import re
 
-client = discord.client()
+client = discord.Client()
 
 @client.event
 async def on_ready():
@@ -19,13 +19,13 @@ async def on_message(message):
 		msg = 'Hello, this bot provides links to subreddits when users enter text of the format /r/x or r/x'.format(message)
 		await client.send_message(message.channel, msg)
 
-	searchMsg = re.findall("\/?r\/[a-Z_]+", message.content)
+	searchMsg = re.findall("\/?r\/[A-z_]+", message.content)
 	if not searchMsg:
 		return
 	else:
-		msg = ('You posted a subreddit in chat, here\'s a quick link to it: \n')
+		msg = 'You posted a subreddit in chat, here\'s a quick link to it: \n'
 		for s in searchMsg:
-			if s[0] = '/':
+			if s[0] == '/':
 				msg += 'http://www.reddit.com' + s + '\n'
 			else:
 				msg += 'http://www.reddit.com/' + s + '\n'
